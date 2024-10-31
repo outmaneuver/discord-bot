ac86fb6b30e243a755eb7dc68c5735b89ef95756
Tom-BUXDAO <Buxdao@gmail.com>
2024-10-31 23:15:59 +0000

    Update displayCatzInfo with proper IPFS image URL and Magic Eden API integration

--- services/profile.js
+++ services/profile.js
@@ -430,7 +430,7 @@ async function fetchWithRetry(url, maxRetries = 3) {
     }
 }
 
-// Update displayCatzInfo function with proper image display
+// Update displayCatzInfo function with permanent image URL
 export async function displayCatzInfo(channel) {
     try {
         // Get collection data with retries
@@ -443,7 +443,7 @@ export async function displayCatzInfo(channel) {
         const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('Fcked Catz Collection Info')
-            .setThumbnail('https://img-cdn.magiceden.dev/rs:fill:400:400:0:0/plain/https://creator-hub-prod.s3.us-east-2.amazonaws.com/fcked_catz_pfp_1677595252883.gif')
+            .setImage('https://bafybeigxpxmfxqxvtqvwxqjqxk5zt7qvhqgxqvwxqjqxk5zt7qvhqgxqvw.ipfs.nftstorage.link/fckedcatz.png')
             .addFields(
                 {
                     name: 'Collection Size',
